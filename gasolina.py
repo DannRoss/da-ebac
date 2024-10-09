9453ffa01a0d906c16a9261fc92013bdf73f883f
dannross <ytcattergames@gmail.com>
2024-10-09 06:37:15 +0000

    Adicionando gasolina.py, gasolina.csv e gasolina.png

---
+++ gasolina.py
@@ -0,0 +1,22 @@
+
+import os
+import pandas as pd
+import seaborn as sns
+
+dados_gasolina = pd.read_csv('gasolina.csv')
+
+with sns.axes_style('darkgrid'):
+  grafico_gasolina = sns.lineplot(
+      x = 'dia',
+      y = 'venda',
+      data = dados_gasolina
+  )
+  
+  grafico_gasolina.set(
+      title = 'Preço médio de venda de gasolina',
+      xlabel = 'Dia',
+      ylabel = 'Valor'
+  )
+  
+  figura = grafico_gasolina.get_figure()    
+  figura.savefig('gasolina.png', dpi=600)
